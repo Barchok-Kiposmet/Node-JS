2126b84c421dae92eda0b69b0cf0b2b1fccef4b0
Barchok-Kiposmet <barchokpatrick@gmail.com>
2022-12-14 21:12:54 +0300

    Refactored the code

--- index.js
+++ index.js
@@ -1,93 +1,101 @@
-// Import
-const http = require("http");
-const url = require("url");
-const StringDecoder = require("string_decoder").StringDecoder;
+// // Import
+// const http = require("http");
+// const url = require("url");
+// const StringDecoder = require("string_decoder").StringDecoder;
 
-// Handlers
-let handlers = {};
+// // Handlers
+// let handlers = {};
 
-handlers.home = function (data, callback) {
-  console.log(data);
-  // TODO: Respond with the home html page;
-  // TODO: Deploy web app -> Heroku
-  callback(200, "<p>This is the home page</p>");
-};
-handlers.about = function (data, callback) {
-  callback(200);
-};
-handlers.notFound = function (data, callback) {
-  callback(404);
-};
+// handlers.home = function (data, callback) {
+//   console.log(data);
+//   // TODO: Respond with the home html page;
+//   // TODO: Deploy web app -> Heroku
+//   callback(200, "<p>This is the home page</p>");
+// };
+// handlers.about = function (data, callback) {
+//   callback(200);
+// };
+// handlers.notFound = function (data, callback) {
+//   callback(404);
+// };
 
-// Router
-const router = {
-  home: handlers.home,
-  about: handlers.about,
-};
+// // Router
+// const router = {
+//   home: handlers.home,
+//   about: handlers.about,
+// };
 
-// Create Server
-const server = http.createServer(function (request, response) {
-  const method = request.method;
-  const requestUrl = request.url;
-  const headers = request.headers;
+// // Create Server
+// const server = http.createServer(function (request, response) {
+//   const method = request.method;
+//   const requestUrl = request.url;
+//   const headers = request.headers;
 
-  // URL Parsing
-  const parsedUrl = url.parse(requestUrl, true);
+//   // URL Parsing
+//   const parsedUrl = url.parse(requestUrl, true);
 
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
-  const query = parsedUrl.query;
+//   const path = parsedUrl.pathname;
+//   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+//   const query = parsedUrl.query;
 
-  // Retrieve the request body
-  const decoder = new StringDecoder("utf-8");
-  let payload = "";
-  request.on("data", function (chunk) {
-    payload += decoder.write(chunk);
-  });
+//   // Retrieve the request body
+//   const decoder = new StringDecoder("utf-8");
+//   let payload = "";
+//   request.on("data", function (chunk) {
+//     payload += decoder.write(chunk);
+//   });
 
-  request.on("end", function () {
-    payload += decoder.end();
+//   request.on("end", function () {
+//     payload += decoder.end();
 
-    let chosenHandler =
-      typeof router[trimmedPath] !== "undefined"
-        ? router[trimmedPath]
-        : handlers.notFound;
+//     let chosenHandler =
+//       typeof router[trimmedPath] !== "undefined"
+//         ? router[trimmedPath]
+//         : handlers.notFound;
 
-    const data = {
-      method,
-      requestUrl,
-      trimmedPath,
-      query,
-      headers,
-      payload,
-    };
+//     const data = {
+//       method,
+//       requestUrl,
+//       trimmedPath,
+//       query,
+//       headers,
+//       payload,
+//     };
 
-    chosenHandler(data, function (statusCode, payload) {
-      // if (typeof(statusCode) == 'number') {
-      //     statusCode = statusCode;
-      // } else {
-      //     statusCode = 200;
-      // }
-      statusCode = typeof statusCode == "number" ? statusCode : 200;
+//     chosenHandler(data, function (statusCode, payload) {
+//       // if (typeof(statusCode) == 'number') {
+//       //     statusCode = statusCode;
+//       // } else {
+//       //     statusCode = 200;
+//       // }
+//       statusCode = typeof statusCode == "number" ? statusCode : 200;
 
-      payload = typeof payload == "object" ? payload : {};
+//       payload = typeof payload == "object" ? payload : {};
 
-      // Convert to string
-      const payloadAsAString = JSON.stringify(payload);
+//       // Convert to string
+//       const payloadAsAString = JSON.stringify(payload);
 
-      response.writeHead(statusCode);
-      response.end(payloadAsAString);
-    });
-  });
-});
+//       response.writeHead(statusCode);
+//       response.end(payloadAsAString);
+//     });
+//   });
+// });
 
-// NB: Use port number ranges between 3000-5000
-server.listen(3000, () => {
-  console.log("The server is running");
-});
+// // NB: Use port number ranges between 3000-5000
+// server.listen(3000, () => {
+//   console.log("The server is running");
+// });
 
 // this starts the server from the server.js file which was wrapped in the start function.
 
 let server = require("./server");
+let router = require("./router");
+let requestHandlers = require("./requestHandlers");
 
-server.start();
+let handle = {};
+handle["/"] = requestHandlers.index;
+handle["/index.html"] = requestHandlers.index;
+handle["/portfolio.html"] = requestHandlers.portfolio;
+
+server.start(router.route, handle);
+
--- server.js
+++ server.js
@@ -1,37 +1,43 @@
-// this creates a server and it does acts on client's requests.
+// // this creates a server and it does acts on client's requests.
 
-let http = require("http");
+// let http = require("http");
 
-http
-  .createServer(function (request, response) {
-    response.writeHead(200, { "content-type": "text/plain" });
-    response.write("Hello world!");
-    response.end();
-  })
-  .listen(8000, () => {
-    console.log("Server is running"); // this is to show on the console once we start the server.
-  });
+// http
+//   .createServer(function (request, response) {
+//     response.writeHead(200, { "content-type": "text/plain" });
+//     response.write("Hello world!");
+//     response.end();
+//   })
+//   .listen(8000, () => {
+//     console.log("Server is running"); // this is to show on the console once we start the server.
+//   });
 
-// Running the server with a refactored code but with the same purpose as one above. Different function
+// // Running the server with a refactored code but with the same purpose as one above. Different function
 
-let http = require("http");
+// let http = require("http");
 
-function onRequest(request, response) {
-  response.writeHead(200, { "Content-type": "text/plain" });
-  response.write("Hello World");
-  response.end();
-}
+// function onRequest(request, response) {
+//   response.writeHead(200, { "Content-type": "text/plain" });
+//   response.write("Hello World");
+//   response.end();
+// }
 
-http.createServer(onRequest).listen(8000);
+// http.createServer(onRequest).listen(8000);
 
 // asynchronus functions, execute before calling the functions.
 
 let http = require("http");
+let url = require('url');
+const { route } = require("./router");
 
 // this wraps the server in a function to be run in an index.js file
-let start = function () {
+let start = function (route, handle) {
   function onRequest(request, response) {
+    let pathname = url.parse(request.url).pathname
+    console.log(pathname);
     console.log("Request received!");
+
+    route(pathname);
     response.writeHead(200, { "Content-type": "text/plain" });
     response.write("Hello World");
     response.end();
