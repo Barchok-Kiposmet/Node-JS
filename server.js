3ee993ba03a9f5c96884bf2619f0ddc14d15ef69
Barchok-Kiposmet <barchokpatrick@gmail.com>
2022-12-14 14:14:09 +0300

    Asynchronus functions

--- server.js
+++ server.js
@@ -22,3 +22,18 @@ function onRequest (request, response){
 }
 
 http.createServer(onRequest).listen(8000);
+
+// asynchronus functions, execute before calling the functions.
+
+let http = require('http');
+
+function onRequest(request, response){
+   console.log("Request received!")
+   response.writeHead(200, {"Content-type": "text/plain"});
+   response.write("Hello World");
+   response.end();
+}
+
+http.createServer(onRequest).listen(8000);
+
+console.log("Server has started.")
