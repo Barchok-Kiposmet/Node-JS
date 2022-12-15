236f7d2d6a1395da8b9ddac457683541ca3b1a37
Barchok-Kiposmet <barchokpatrick@gmail.com>
2022-12-15 10:02:42 +0300

    Deleted unwanted files for the app

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ let start = function (route, handle) {
     response.writeHead(200, { "Content-type": "text/plain" });
     response.write("Hello World");
     response.end();
-  }
+  };
 
   http.createServer(onRequest).listen(8000);
 
